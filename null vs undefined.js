912e90f1fad7b6860018fc40ae7b9df9182a8440
SAabdullah31 <cse.asabdullah@gmail.com>
2020-03-07 15:07:44 +0600

    null vs undefined interview question

---
+++ null vs undefined.js
@@ -0,0 +1,18 @@
+//let phaki;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return
+// }
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+// const pemikManush={name:"abdullah", phone:01759655598};
+
+// console.log(pemikManush.gf);
+
+
+// let fun =undefined;
+// console.log(fun);
+
+let ages=[2,4,5];
+console.log(ages[9]);
