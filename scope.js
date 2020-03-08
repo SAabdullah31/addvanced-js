4a4ae86b22f279829aa3f3573492d8d29e3727a4
SAabdullah31 <cse.asabdullah@gmail.com>
2020-03-08 22:06:47 +0600

    practing some advaced of javascript

---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus =20;
+function sum(first,second){
+    let result = first + second +bonus ;
+    //.log(bonus);
+    if(result>9){
+        let mood ="Happy";
+        mood ="fisky";
+        mood ="funcky";
+        mood ="manky";
+        console.log(mood);
+    }
+    return result;
+}
+console.log(mood);
+const output = sum(3,7);
+//console.log(bonus);
+console.log(output);
